44db6a896cf4bccb82f72836477713f2aae48ed3
jiahuan.yu <461866620@qq.com>
2015-04-02 17:08:03 +0800

    add some helper

---
+++ library/src/main/java/com/jiahuan/svgmapview/library/core/helper/ImageHelper.java
@@ -0,0 +1,116 @@
+package com.jiahuan.svgmapview.library.core.helper;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.Drawable;
+
+public class ImageHelper
+{
+	public static Bitmap drawableToBitmap(Drawable drawable)
+	{
+		int width = drawable.getIntrinsicWidth();
+		int height = drawable.getIntrinsicHeight();
+		Bitmap bitmap = Bitmap.createBitmap(width, height, drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
+		Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, width, height);
+		drawable.draw(canvas);
+		return bitmap;
+	}
+
+	/**
+	 * 正方形数据
+	 * 
+	 * @param drawable
+	 * @param dipValue
+	 * @param context
+	 * @return
+	 */
+	public static Bitmap drawableToBitmap(Drawable drawable, int size)
+	{
+		int width = size;
+		int height = size;
+		int ratio = size / drawable.getIntrinsicHeight();
+		Bitmap bitmap = Bitmap.createBitmap(width, height, drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
+		Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, width, height);
+		canvas.scale(ratio, ratio);
+		drawable.draw(canvas);
+		return bitmap;
+	}
+
+	public static Bitmap drawableToBitmap(Drawable drawable, float ratio)
+	{
+		int width = (int) Math.ceil(drawable.getIntrinsicWidth() * ratio);
+		int height = (int) Math.ceil(drawable.getIntrinsicHeight() * ratio);
+		Bitmap bitmap = Bitmap.createBitmap(width, height, drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888 : Bitmap.Config.RGB_565);
+		Canvas canvas = new Canvas(bitmap);
+		canvas.scale(ratio, ratio);
+		drawable.setBounds(0, 0, width, height);
+		drawable.draw(canvas);
+		return bitmap;
+	}
+
+	public static int[][] getImageARGB(Bitmap bitmap)
+	{
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+		final int[][] rgbArray = new int[width][height];
+		for (int i = 0; i < width; i++)
+		{
+			for (int j = 0; j < height; j++)
+			{
+				rgbArray[i][j] = bitmap.getPixel(i, j);
+			}
+		}
+		return rgbArray;
+	}
+
+	public static Bitmap getBitmapFromRemote(String address)
+	{
+		Bitmap bitmap = null;
+		try
+		{
+			URL url = new URL(address);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoInput(true);
+			conn.connect();
+			InputStream is = conn.getInputStream();
+			bitmap = BitmapFactory.decodeStream(is);
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		return bitmap;
+	}
+
+	public static InputStream getIS(String address)
+	{
+		InputStream is = null;
+		try
+		{
+			URL url = new URL(address);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoInput(true);
+			conn.connect();
+			is = conn.getInputStream();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return is;
+	}
+}
