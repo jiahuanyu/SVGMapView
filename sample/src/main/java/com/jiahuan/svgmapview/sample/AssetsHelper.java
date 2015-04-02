e18894c0dc0f2b8a71e7aae7bcabbf047cc3e718
jiahuan.yu <461866620@qq.com>
2015-04-02 16:45:08 +0800

    make a simple sample

---
+++ sample/src/main/java/com/jiahuan/svgmapview/sample/AssetsHelper.java
@@ -0,0 +1,33 @@
+package com.jiahuan.svgmapview.sample;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by doom on 15/4/2.
+ */
+public class AssetsHelper
+{
+    public static String getContent(Context context, String fileName)
+    {
+        try
+        {
+            InputStreamReader inputReader = new InputStreamReader(context.getResources().getAssets().open(fileName));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            String Result = "";
+            while ((line = bufReader.readLine()) != null)
+                Result += line;
+
+            System.out.print(Result);
+            return Result;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
--- sample/src/main/java/com/jiahuan/svgmapview/sample/MainActivity.java
+++ sample/src/main/java/com/jiahuan/svgmapview/sample/MainActivity.java
@@ -1,44 +1,26 @@
 package com.jiahuan.svgmapview.sample;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+
+import com.jiahuan.svgmapview.library.SVGMapView;
 
 
 public class MainActivity extends ActionBarActivity
 {
+    private SVGMapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mapView = (SVGMapView) findViewById(R.id.mapView);
+
+        mapView.loadMap(AssetsHelper.getContent(this, "sample2.svg"));
+
+        mapView.getController().setRotationGestureEnabled(false);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
