2a75353a2f535de8f6f9c8526191f2cbf1922392
jiahuan.yu <461866620@qq.com>
2015-05-22 13:22:45 +0800

    Delete unuse

--- library/src/main/java/com/jiahuan/svgmapview/core/helper/map/SVGBuilder.java
+++ library/src/main/java/com/jiahuan/svgmapview/core/helper/map/SVGBuilder.java
@@ -1,7 +1,5 @@
 package com.jiahuan.svgmapview.core.helper.map;
 
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.graphics.ColorFilter;
 import android.util.Log;
 
@@ -31,19 +29,6 @@ public class SVGBuilder
     private boolean closeInputStream = true;
 
     /**
-     * Parse SVG data from an input stream.
-     *
-     * @param svgData the input stream, with SVG XML data in UTF-8 character
-     *                encoding.
-     * @return the parsed SVG.
-     */
-    public SVGBuilder readFromInputStream(InputStream svgData)
-    {
-        this.data = svgData;
-        return this;
-    }
-
-    /**
      * Parse SVG data from a string.
      *
      * @param svgData the string containing SVG XML data.
@@ -51,112 +36,6 @@ public class SVGBuilder
     public SVGBuilder readFromString(String svgData)
     {
         this.data = new ByteArrayInputStream(svgData.getBytes());
-        return this;
-    }
-
-    /**
-     * Parse SVG data from an Android application resource.
-     *
-     * @param resources the Android context resources.
-     * @param resId     the ID of the raw resource SVG.
-     */
-    public SVGBuilder readFromResource(Resources resources, int resId)
-    {
-        this.data = resources.openRawResource(resId);
-        return this;
-    }
-
-    /**
-     * Parse SVG data from an Android application asset.
-     *
-     * @param assetMngr the Android asset manager.
-     * @param svgPath   the path to the SVG file in the application's assets.
-     * @throws java.io.IOException if there was a problem reading the file.
-     */
-    public SVGBuilder readFromAsset(AssetManager assetMngr, String svgPath) throws IOException
-    {
-        this.data = assetMngr.open(svgPath);
-        return this;
-    }
-
-    public SVGBuilder clearColorSwap()
-    {
-        searchColor = replaceColor = null;
-        return this;
-    }
-
-    /**
-     * Replaces a single colour with another.
-     *
-     * @param searchColor  The colour in the SVG.
-     * @param replaceColor The desired colour.
-     */
-    public SVGBuilder setColorSwap(int searchColor, int replaceColor)
-    {
-        return setColorSwap(searchColor, replaceColor, false);
-    }
-
-    /**
-     * Replaces a single colour with another, affecting the opacity.
-     *
-     * @param searchColor    The colour in the SVG.
-     * @param replaceColor   The desired colour.
-     * @param overideOpacity If true, combines the opacity defined in the SVG resource with
-     *                       the alpha of replaceColor.
-     */
-    public SVGBuilder setColorSwap(int searchColor, int replaceColor, boolean overideOpacity)
-    {
-        this.searchColor = searchColor;
-        this.replaceColor = replaceColor;
-        this.overideOpacity = overideOpacity;
-        return this;
-    }
-
-    /**
-     * In white-mode, fills are drawn in white and strokes are not drawn at all.
-     */
-    public SVGBuilder setWhiteMode(boolean whiteMode)
-    {
-        this.whiteMode = whiteMode;
-        return this;
-    }
-
-    /**
-     * Applies a {@link android.graphics.ColorFilter} to the paint objects used to render the
-     * SVG.
-     */
-    public SVGBuilder setColorFilter(ColorFilter colorFilter)
-    {
-        this.strokeColorFilter = this.fillColorFilter = colorFilter;
-        return this;
-    }
-
-    /**
-     * Applies a {@link android.graphics.ColorFilter} to strokes in the SVG.
-     */
-    public SVGBuilder setStrokeColorFilter(ColorFilter colorFilter)
-    {
-        this.strokeColorFilter = colorFilter;
-        return this;
-    }
-
-    /**
-     * Applies a {@link android.graphics.ColorFilter} to fills in the SVG.
-     */
-    public SVGBuilder setFillColorFilter(ColorFilter colorFilter)
-    {
-        this.fillColorFilter = colorFilter;
-        return this;
-    }
-
-    /**
-     * Whether or not to close the input stream after reading (ie. after calling
-     * {@link #build()}.<br>
-     * <em>(default is true)</em>
-     */
-    public SVGBuilder setCloseInputStreamWhenDone(boolean closeInputStream)
-    {
-        this.closeInputStream = closeInputStream;
         return this;
     }
 
